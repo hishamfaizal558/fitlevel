eaebb04306da9f67de459bf85bbaab1a47e290a5
hishamfaizal558 <hishamfaizal558@gmail.com>
2025-10-07 18:55:32 +0530

    Initial commit

--- app.py
+++ app.py
@@ -168,7 +168,12 @@ def diet():
     return render_template("diet.html", result=result)
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 # Remove Jinja template code from Python file.
